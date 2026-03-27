733ae91bda465e9ac6ad14e96f9fa4fa71c959cd
Nuutti <nuutti.kyllonen.2006@gmail.com>
2026-03-27 21:02:06 +0200

    updates

--- backend/routers.py
+++ backend/routers.py
@@ -1,12 +1,20 @@
+from google import genai
 from fastapi import FastAPI, HTTPException, APIRouter
-
+import os
+from dotenv import load_dotenv
+load_dotenv()
 
 router = APIRouter()
+client = genai.Client(api_key=os.getenv("GEMINI_API_KEY"))
 
 
-@router.post("/verify", tags=["Verify"])
-async def verify_text(text: str):
-    return {"message": "Here we check your text.", "received": text}
+@router.post("/chat", tags=["Verify"])
+async def verify_text(user_message: str):
+    response = client.models.generate_content(
+        model="gemini-3-flash-preview",
+        contents=user_message
+    )
+    return {"AI expalin":  response.text}
 
 
 @router.get("/Welcome", tags=["Welcome"])
