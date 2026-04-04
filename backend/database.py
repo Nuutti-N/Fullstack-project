0e124097f3a46c58576943cf7e06831ea76e0294
Nuutti <nuutti.kyllonen.2006@gmail.com>
2026-04-04 12:09:50 +0300

    updates

--- backend/database.py
+++ backend/database.py
@@ -3,18 +3,13 @@ import os
 from dotenv import load_dotenv
 load_dotenv()
 
-# database_url = os.getenv("database_url")
-# if not database_url:
-#     raise ValueError("Check your .env file")
-# engine = create_engine(database_url, echo=True)
-# SQLModel.metadata.create_all(engine)
-
-
-# def get_session():
-#     with Session(engine) as session:
-#         yield session
+database_url = os.getenv("database_url")
+if not database_url:
+    raise ValueError("Check your .env file")
+engine = create_engine(database_url, echo=True)
+SQLModel.metadata.create_all(engine)
 
 
 def get_session():
-
-    return None
+    with Session(engine) as session:
+        yield session
