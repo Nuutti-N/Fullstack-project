b90cd81af3c1cd3a4cd3c0766b9de07efdd0766b
Nuutti <nuutti.kyllonen.2006@gmail.com>
2026-04-09 09:21:58 +0300

    updates

--- backend/tests/conftest.py
+++ backend/tests/conftest.py
@@ -1,18 +1,24 @@
+from backend import models
+from backend.database import engine
+from sqlmodel import SQLModel
 import os
 import pytest
 from fastapi.testclient import TestClient
 
 
-# @pytest.fixture(scope="session", autouse=True)
-# def set_deest_env():
-#     os.environ.setdefault(
-#         "database_url", "test_database_what_you_use")
-#     os.environ.setdefault("jwt_key", "test_jwt_key")
-#     os.environ.setdefault("jwt_refresh_key", "test_jwt_refresh_key")
-#     os.environ.setdefault("algorithm", "HS256")
-#     os.environ.setdefault("supabase_url", "http://localhost:54321")
-#     os.environ.setdefault("supabase_key", "test_supabase_key")
-#     os.environ.setdefault("gemini_api_key", "test_gemini_api_key")
+@pytest.fixture(scope="session", autouse=True)
+def set_deest_env():
+    os.environ["database_url"] = "postgresql+psycopg2://postgres:postgres@localhost:5433/test_db"
+    os.environ["jwt_key"] = "test_jwt_key"
+    os.environ["jwt_refresh_key"] = "test_jwt_refresh_key"
+    os.environ["algorithm"] = "HS256"
+    os.environ["supabase_url"] = "http://localhost:54321"
+    os.environ["supabase_key"] = "test_supabase_key"
+    os.environ["gemini_api_key"] = "test_gemini_api_key"
+
+
+SQLModel.metadata.drop_all(engine)
+SQLModel.metadata.create_all(engine)
 
 
 @pytest.fixture()
--- backend/tests/test_routers.py
+++ backend/tests/test_routers.py
@@ -1,8 +1,14 @@
 
-
 def test_welcome(client):
     response = client.get("/welcome")
     assert response.status_code == 200
     assert response.json() == {
         "message": "Welcome to the AI Agent"
     }
+
+
+def test_signup(client):
+    response = client.post(
+        "/signup", json={"username": "testuser", "password": "testpass"})
+    assert response.status_code == 200
+    assert response.json()["detail"] == "Username exists"
