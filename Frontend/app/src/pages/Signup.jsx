7f56fb34dfcec30d29b8e338c3267b1e2e61c4ce
Nuutti <nuutti.kyllonen.2006@gmail.com>
2026-04-26 14:57:58 +0300

    add login classNames to signup, and polish signup page

--- Frontend/app/src/pages/Signup.jsx
+++ Frontend/app/src/pages/Signup.jsx
@@ -5,6 +5,7 @@ import { Eye, EyeOff, ArrowRight } from "lucide-react"
 
 function Signup() {
     const navigate = useNavigate()
+    const [showPassword, setShowPassword] = useState(false)
     const [error, setError] = useState("")
     const [username, setUsername] = useState("")
     const [password, setPassword] = useState("")
@@ -15,26 +16,59 @@ function Signup() {
             navigate("/login")
         }
         catch (err) {
-
+            setError("username or password invalid")
         }
 
     }
     return (
-        <form className="auth-page" onSubmit={handleSubmit}>
-            <input
-                type="text"
-                value={username}
-                onChange={e => setUsername(e.target.value)}
-            />
-            <input
-                type="password"
-                value={password}
-                onChange={e => setPassword(e.target.value)}
-            />
-            <button type="submit">Sign up</button>
-            <p>Already have an account?</p>
-            <Link to="/login" className="auth-link">Log in</Link>
-        </form>
+        <div className="auth-page">
+            <form className="auth-form" onSubmit={handleSubmit}>
+                <header className="auth-header">
+                    <h1 className="auth-title">Create  account</h1>
+                    <p className="auth-subtitle">Start fact-checking AI today</p>
+                </header>
+                <div className="username-row">
+                    <label htmlFor="username">Username</label>
+                </div>
+                <input
+                    id="username"
+                    type="text"
+                    placeholder="username"
+                    className="username-input"
+                    value={username}
+                    onChange={e => setUsername(e.target.value)}
+                />
+                <div className="password-row">
+                    <label htmlFor="password">Password</label>
+                </div>
+                <div className="password-wrapper">
+                    <input
+                        id="password"
+                        type={showPassword ? "text" : "password"}
+                        placeholder="••••••••"
+                        value={password}
+                        className="password-input"
+                        onChange={e => setPassword(e.target.value)}
+                        minLength={8}
+                    />
+                    <button
+                        type="button"
+                        className="hide-button"
+                        onClick={() => setShowPassword(!showPassword)}
+                    >{showPassword ? <EyeOff /> : <Eye />}</button>
+                </div>
+                {
+                    error && <p className="auth-error">{error}</p>
+                }
+                <button type="submit" className="auth-submit">Sign up
+                    <ArrowRight />
+                </button>
+                <div className="auth-toggle">
+                    <p>Already have an account?</p>
+                    <Link to="/login" className="auth-link">Log in</Link>
+                </div>
+            </form>
+        </div>
 
     )
 }
