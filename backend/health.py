09f8452d1fbd465dcfd2b373ed2194808e4875ea
Nuutti <nuutti.kyllonen.2006@gmail.com>
2026-04-26 12:16:54 +0300

    add health, which say I am alive, and app work external

---
+++ backend/health.py
@@ -0,0 +1,11 @@
+
+from backend.models import HealthCheck
+from fastapi import APIRouter, status
+
+router = APIRouter()
+
+
+@router.get("/health", tags=["healthcheck"], summary="Perform a healthcheck",
+            response_description="Return HTTP status code 200 (OK)", status_code=status.HTTP_200_OK, response_model=HealthCheck)
+def get_health() -> HealthCheck:
+    return HealthCheck(status="OK")
