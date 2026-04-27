4db435ebc18ba409e0058c12606098a71716a936
Nuutti <nuutti.kyllonen.2006@gmail.com>
2026-04-27 21:59:29 +0300

    add test_refresh, that we see if something breaks when access_token expired

--- backend/tests/test_routers.py
+++ backend/tests/test_routers.py
@@ -30,3 +30,17 @@ def test_delete_history_request_token(client):
     response = client.delete("/delete_history/{fact_id}")
     assert response.status_code == 401
     assert response.json()["detail"] == "Not authenticated"
+
+
+def test_chat_with_token(client, auth_token):
+    access_token, refresh_token = auth_token
+    response = client.get(
+        "/your", headers={"Authorization": f"Bearer {access_token}"})
+    assert response.status_code == 200
+
+
+def test_refresh_with_token(client, auth_token):
+    access_token, refresh_token = auth_token
+    response = client.post(
+        "/refresh", headers={"Authorization": f"Bearer {access_token}"}, json={"refresh_token": refresh_token})
+    assert response.status_code == 200
