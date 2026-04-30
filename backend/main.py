f9acc6dcd1265fd999e68ba92d62c0c6247d6a10
Nuutti <nuutti.kyllonen.2006@gmail.com>
2026-04-30 14:11:54 +0300

    add vercel url to cors

--- backend/main.py
+++ backend/main.py
@@ -27,7 +27,8 @@ app.add_middleware(
     allow_origins=[
         "http://localhost:3000",
         "http://localhost:5173",
-        # settings.VITE_API_URL,
+        "http://fullstack-project-nine-chi.vercel.app"
+
     ],
     allow_credentials=True,
     allow_methods=["GET", "DELETE", "POST"],
