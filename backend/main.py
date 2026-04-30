3d4c95aadb088257a11e6babe982fd8767624800
Nuutti <nuutti.kyllonen.2006@gmail.com>
2026-04-30 23:54:12 +0300

    delete again solve problem

--- backend/main.py
+++ backend/main.py
@@ -27,7 +27,6 @@ app.add_middleware(
     allow_origins=[
         "http://localhost:3000",
         "http://localhost:5173",
-        "https://fullstack-project-nine-chi.vercel.app",
     ],
     allow_credentials=True,
     allow_methods=["GET", "DELETE", "POST"],
